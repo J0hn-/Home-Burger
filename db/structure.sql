d1168c95e82427a914b3661ade26743cf74d2e4a
zominym <marc.zominy@wanadoo.fr>
2015-03-13 15:58:06 +0100

    Debut Database

--- db/structure.sql
+++ db/structure.sql
@@ -1,11 +1,13 @@
-drop table if exists t_comment;
+drop table if exists t_categorie;
 drop table if exists t_user;
-drop table if exists t_article;
+drop table if exists t_burger;
 
-create table t_article (
-    art_id integer not null primary key auto_increment,
-    art_title varchar(100) not null,
-    art_content varchar(2000) not null
+create table t_burger (
+    brg_id integer not null primary key auto_increment,
+    brg_name varchar(50) not null,
+    brg_resume varchar(2000) not null,
+	brg_cat integer,
+	constraint fk_cat foreign key(brg_cat) references t_categorie(cat_id)
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table t_user (
@@ -16,10 +18,10 @@ create table t_user (
     usr_role varchar(50) not null 
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
-create table t_comment (
-    com_id integer not null primary key auto_increment,
-    com_content varchar(500) not null,
-    art_id integer not null,
+create table t_categorie (
+    cat_id integer not null primary key auto_increment,
+    cat_name varchar(50) not null,
+    brg_id integer not null,
     usr_id integer not null,
     constraint fk_com_art foreign key(art_id) references t_article(art_id),
     constraint fk_com_usr foreign key(usr_id) references t_user(usr_id)
