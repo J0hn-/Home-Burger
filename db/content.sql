db5fcbc68464cdc2ea10fd1e70829e04d9e491f7
zominym <marc.zominy@wanadoo.fr>
2015-03-17 11:07:32 +0100

    Database des burgers terminée
    Plus tard on ajoutera les commentaires et tout.
    Un burger n'a plus de catégorie mais il y a une table (burger,
    categorie) permettant de mettre plusieurs burgers par categories et
    plusieurs categories par burger.
    Même système pour les ingrédients.

--- db/content.sql
+++ db/content.sql
@@ -1,9 +1,81 @@
-insert into t_article values
-(1, 'First article', 'Hi there! This is the very first article.');
-insert into t_article values
-(2, 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit mauris ac porttitor accumsan. Nunc vitae pulvinar odio, auctor interdum dolor. Aenean sodales dui quis metus iaculis, hendrerit vulputate lorem vestibulum. Suspendisse pulvinar, purus at euismod semper, nulla orci pulvinar massa, ac placerat nisi urna eu tellus. Fusce dapibus rutrum diam et dictum. Sed tellus ipsum, ullamcorper at consectetur vitae, gravida vel sem. Vestibulum pellentesque tortor et elit posuere vulputate. Sed et volutpat nunc. Praesent nec accumsan nisi, in hendrerit nibh. In ipsum mi, fermentum et eleifend eget, eleifend vitae libero. Phasellus in magna tempor diam consequat posuere eu eget urna. Fusce varius nulla dolor, vel semper dui accumsan vitae. Sed eget risus neque.');
-insert into t_article values
-(3, 'Lorem ipsum in french', "J’en dis autant de ceux qui, par mollesse d’esprit, c’est-à-dire par la crainte de la peine et de la douleur, manquent aux devoirs de la vie. Et il est très facile de rendre raison de ce que j’avance. Car, lorsque nous sommes tout à fait libres, et que rien ne nous empêche de faire ce qui peut nous donner le plus de plaisir, nous pouvons nous livrer entièrement à la volupté et chasser toute sorte de douleur ; mais, dans les temps destinés aux devoirs de la société ou à la nécessité des affaires, souvent il faut faire divorce avec la volupté, et ne se point refuser à la peine. La règle que suit en cela un homme sage, c’est de renoncer à de légères voluptés pour en avoir de plus grandes, et de savoir supporter des douleurs légères pour en éviter de plus fâcheuses.");
+insert into t_cat values
+(1, 'Classiques');
+insert into t_cat values
+(2, 'Épicé');
+insert into t_cat values
+(3, 'Végétarien');
+insert into t_cat values
+(4, 'Végétalien');
+insert into t_cat values
+(5, 'Boeuf');
+insert into t_cat values
+(6, 'Poulet');
+insert into t_cat values
+(7, 'Poisson');
+
+
+insert into t_brg values
+(1, 'Cheeseburger', "L'incontournable cheeseburger !");
+insert into t_brg values
+(2, 'Hamburger', "Rien de plus standart !");
+insert into t_brg values
+(3, 'Eggburger', "Pour vous les végétariens !");
+insert into t_brg values
+(4, 'Vegetoburger', "Pour les plus stricts végétaliens !");
+insert into t_brg values
+(5, 'Burnburger', "Appelez les pompiers !");
+insert into t_brg values
+(6, 'Chickenburger', "Cot cot cot !");
+insert into t_brg values
+(7, 'Fishburger', "Magicarp ! Magicarp !");
+
+
+insert into t_brg_cat values (1, 1);
+insert into t_brg_cat values (1, 5);
+insert into t_brg_cat values (2, 1);
+insert into t_brg_cat values (2, 5);
+insert into t_brg_cat values (3, 3);
+insert into t_brg_cat values (4, 4);
+insert into t_brg_cat values (5, 2);
+insert into t_brg_cat values (5, 5);
+insert into t_brg_cat values (6, 6);
+insert into t_brg_cat values (6, 2);
+insert into t_brg_cat values (7, 7);
+
+
+insert into t_ing values (1, 'Cornichons');
+insert into t_ing values (2, 'Salade, Tomates, Oignons');
+insert into t_ing values (3, 'Fromage');
+insert into t_ing values (4, 'Steak');
+insert into t_ing values (5, 'Poulet');
+insert into t_ing values (6, 'Poisson');
+insert into t_ing values (7, 'Oeuf');
+insert into t_ing values (8, 'Steak Végétarien');
+insert into t_ing values (9, 'Piment');
+
+
+insert into t_brg_ing values (1, 1);
+insert into t_brg_ing values (1, 2);
+insert into t_brg_ing values (1, 3);
+insert into t_brg_ing values (1, 4);
+insert into t_brg_ing values (2, 1);
+insert into t_brg_ing values (2, 2);
+insert into t_brg_ing values (2, 4);
+insert into t_brg_ing values (3, 2);
+insert into t_brg_ing values (3, 3);
+insert into t_brg_ing values (3, 7);
+insert into t_brg_ing values (4, 2);
+insert into t_brg_ing values (4, 8);
+insert into t_brg_ing values (5, 2);
+insert into t_brg_ing values (5, 4);
+insert into t_brg_ing values (5, 9);
+insert into t_brg_ing values (6, 2);
+insert into t_brg_ing values (6, 3);
+insert into t_brg_ing values (6, 5);
+insert into t_brg_ing values (7, 1);
+insert into t_brg_ing values (7, 2);
+insert into t_brg_ing values (7, 6);
+
 
 /* raw password is 'john' */
 insert into t_user values
@@ -11,12 +83,15 @@ insert into t_user values
 /* raw password is 'jane' */
 insert into t_user values
 (2, 'JaneDoe', 'EfakNLxyhHy2hVJlxDmVNl1pmgjUZl99gtQ+V3mxSeD8IjeZJ8abnFIpw9QNahwAlEaXBiQUBLXKWRzOmSr8HQ==', 'dhMTBkzwDKxnD;4KNs,4ENy', 'ROLE_USER');
+/* raw password is '@dm1n' */
+insert into t_user values
+(3, 'admin', 'gqeuP4YJ8hU3ZqGwGikB6+rcZBqefVy+7hTLQkOD+jwVkp4fkS7/gr1rAQfn9VUKWc7bvOD7OsXrQQN5KGHbfg==', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
 
+
+/*
 insert into t_comment values
 (1, 'Great! Keep up the good work.', 1, 1);
 insert into t_comment values
 (2, "Thank you, I'll try my best.", 1, 2);
+*/
 
-/* raw password is '@dm1n' */
-insert into t_user values
-(3, 'admin', 'gqeuP4YJ8hU3ZqGwGikB6+rcZBqefVy+7hTLQkOD+jwVkp4fkS7/gr1rAQfn9VUKWc7bvOD7OsXrQQN5KGHbfg==', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
--- db/structure.sql
+++ db/structure.sql
@@ -1,28 +1,42 @@
-drop table if exists t_categorie;
-drop table if exists t_user;
-drop table if exists t_burger;
+drop table if exists t_usr;
+drop table if exists t_cat;
+drop table if exists t_brg;
+drop table if exists t_brg_cat;
+drop table if exists t_ing;
+drop table if exists t_brg_ing;
 
-create table t_burger (
-    brg_id integer not null primary key auto_increment,
+create table t_bur (
+    brg_id integer not null primary key,
     brg_name varchar(50) not null,
     brg_resume varchar(2000) not null,
-	brg_cat integer,
-	constraint fk_cat foreign key(brg_cat) references t_categorie(cat_id)
+	constraint fk_cat foreign key(brg_cat) references t_categorie(cat_id),
+	brg_img_path varchar(50) not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
-create table t_user (
+create table t_cat (
+    cat_id integer not null primary key,
+    cat_name varchar(50) not null,
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table t_brg_cat (
+	brg_id integer not null,
+	cat_id integer not null
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table t_ing (
+	ing_id integer not null,
+	ing_name varchar(50)
+);
+
+create table t_brg_ing (
+	brg_id integer not null,
+	ing_id integer not null
+)engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table t_usr (
     usr_id integer not null primary key auto_increment,
     usr_name varchar(50) not null,
     usr_password varchar(88) not null,
     usr_salt varchar(23) not null,
     usr_role varchar(50) not null 
-) engine=innodb character set utf8 collate utf8_unicode_ci;
-
-create table t_categorie (
-    cat_id integer not null primary key auto_increment,
-    cat_name varchar(50) not null,
-    brg_id integer not null,
-    usr_id integer not null,
-    constraint fk_com_art foreign key(art_id) references t_article(art_id),
-    constraint fk_com_usr foreign key(usr_id) references t_user(usr_id)
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
